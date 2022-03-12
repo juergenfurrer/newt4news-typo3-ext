a5ba71b521a37597143b8d853536cd12352c413e
Juergen Furrer <juergen.furrer@gmail.com>
2022-03-12 12:28:02 +0100

    Fix localconf

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,3 +1,15 @@
 <?php
 defined('TYPO3') || die();
 
+call_user_func(
+    function ($extKey) {
+        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['Newt']['Implementation'][] = \Infonique\Newt4News\Newt\NewsEndpoint::class;
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            $extKey,
+            'Configuration/TypoScript',
+            'Newt4News Configuration'
+        );
+    },
+    $_EXTKEY ?? 'newt4news'
+);
