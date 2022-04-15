26d7c7cc2c7154018fef471491c012aa141c707b
Juergen Furrer <juergen.furrer@gmail.com>
2022-04-15 17:14:00 +0200

    Change depends max

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,8 +12,8 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.0-11.5.99',
-            'newt' => '2.1.0-',
-            'news' => '8.0.0-',
+            'newt' => '2.1.0-2.1.99',
+            'news' => '8.0.0-9.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
